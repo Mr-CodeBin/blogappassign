709e764f39939c7aaa1d16c9cabafde8130ca26a
Mr-CodeBin <nikhilsingh12032@gmail.com>
2024-06-04 00:37:20 +0530

    first commit

---
+++ .dart_tool/flutter_build/98b46815cb1737279ed5b11db8a0c938/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\NICK\ PROJECT\\MainProjects\\blogapp\\.dart_tool\\flutter_build\\98b46815cb1737279ed5b11db8a0c938\\native_assets.yaml: 
---
+++ build/app/generated/source/buildConfig/debug/com/example/blogapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.blogapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.blogapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
